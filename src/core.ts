6da6da953b77d0f6af084671afb2612eba37a04c
Kwang Yul Seo <kseo@kodebox.io>
2017-11-02 11:55:34 +0900

    Add applyMixins helper function

--- src/core.ts
+++ src/core.ts
@@ -130,3 +130,11 @@ export function clone<T>(object: T): T {
     }
     return result;
 }
+
+export function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
