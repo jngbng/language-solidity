7451ccb00ac01c3c869c2ac8ffd2ade7cc703bc2
Kwang Yul Seo <kseo@kodebox.io>
2017-11-10 13:45:00 +0900

    Add path utility functions

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -1,4 +1,4 @@
-import { Map, MapCtr, MapLike, hasOwnProperty } from "./types";
+import { CharacterCodes, Map, MapCtr, MapLike, Path, hasOwnProperty } from "./types";
 export const enum AssertionLevel {
     None = 0,
     Normal = 1,
@@ -272,3 +272,141 @@ export function returnFalse(): false { return false; }
 
 /** Do nothing and return true */
 export function returnTrue(): true { return true; }
+
+export function createGetCanonicalFileName(useCaseSensitiveFileNames: boolean): (fileName: string) => string {
+    return useCaseSensitiveFileNames
+        ? ((fileName) => fileName)
+        : ((fileName) => fileName.toLowerCase());
+}
+
+export function isRootedDiskPath(path: string) {
+    return path && getRootLength(path) !== 0;
+}
+
+/**
+ * Returns length of path root (i.e. length of "/", "x:/", "//server/share/, file:///user/files")
+ */
+export function getRootLength(path: string): number {
+    if (path.charCodeAt(0) === CharacterCodes.slash) {
+        if (path.charCodeAt(1) !== CharacterCodes.slash) return 1;
+        const p1 = path.indexOf("/", 2);
+        if (p1 < 0) return 2;
+        const p2 = path.indexOf("/", p1 + 1);
+        if (p2 < 0) return p1 + 1;
+        return p2 + 1;
+    }
+    if (path.charCodeAt(1) === CharacterCodes.colon) {
+        if (path.charCodeAt(2) === CharacterCodes.slash) return 3;
+        return 2;
+    }
+    // Per RFC 1738 'file' URI schema has the shape file://<host>/<path>
+    // if <host> is omitted then it is assumed that host value is 'localhost',
+    // however slash after the omitted <host> is not removed.
+    // file:///folder1/file1 - this is a correct URI
+    // file://folder2/file2 - this is an incorrect URI
+    if (path.lastIndexOf("file:///", 0) === 0) {
+        return "file:///".length;
+    }
+    const idx = path.indexOf("://");
+    if (idx !== -1) {
+        return idx + "://".length;
+    }
+    return 0;
+}
+
+export function toPath(fileName: string, basePath: string, getCanonicalFileName: (path: string) => string): Path {
+    const nonCanonicalizedPath = isRootedDiskPath(fileName)
+        ? normalizePath(fileName)
+        : getNormalizedAbsolutePath(fileName, basePath);
+    return <Path>getCanonicalFileName(nonCanonicalizedPath);
+}
+
+export function normalizePath(path: string): string {
+    return normalizePathAndParts(path).path;
+}
+
+export function normalizePathAndParts(path: string): { path: string, parts: string[] } {
+    path = normalizeSlashes(path);
+    const rootLength = getRootLength(path);
+    const root = path.substr(0, rootLength);
+    const parts = getNormalizedParts(path, rootLength);
+    if (parts.length) {
+        const joinedParts = root + parts.join(directorySeparator);
+        return { path: pathEndsWithDirectorySeparator(path) ? joinedParts + directorySeparator : joinedParts, parts };
+    }
+    else {
+        return { path: root, parts };
+    }
+}
+
+export function normalizeSlashes(path: string): string {
+    return path.replace(/\\/g, "/");
+}
+
+/**
+ * Internally, we represent paths as strings with '/' as the directory separator.
+ * When we make system calls (eg: LanguageServiceHost.getDirectory()),
+ * we expect the host to correctly handle paths in our specified format.
+ */
+export const directorySeparator = "/";
+const directorySeparatorCharCode = CharacterCodes.slash;
+function getNormalizedParts(normalizedSlashedPath: string, rootLength: number): string[] {
+    const parts = normalizedSlashedPath.substr(rootLength).split(directorySeparator);
+    const normalized: string[] = [];
+    for (const part of parts) {
+        if (part !== ".") {
+            if (part === ".." && normalized.length > 0 && lastOrUndefined(normalized) !== "..") {
+                normalized.pop();
+            }
+            else {
+                // A part may be an empty string (which is 'falsy') if the path had consecutive slashes,
+                // e.g. "path//file.ts".  Drop these before re-joining the parts.
+                if (part) {
+                    normalized.push(part);
+                }
+            }
+        }
+    }
+
+    return normalized;
+}
+
+export function getNormalizedAbsolutePath(fileName: string, currentDirectory: string) {
+    return getNormalizedPathFromPathComponents(getNormalizedPathComponents(fileName, currentDirectory));
+}
+
+export function getNormalizedPathFromPathComponents(pathComponents: ReadonlyArray<string>) {
+    if (pathComponents && pathComponents.length) {
+        return pathComponents[0] + pathComponents.slice(1).join(directorySeparator);
+    }
+}
+
+export function getNormalizedPathComponents(path: string, currentDirectory: string) {
+    path = normalizeSlashes(path);
+    let rootLength = getRootLength(path);
+    if (rootLength === 0) {
+        // If the path is not rooted it is relative to current directory
+        path = combinePaths(normalizeSlashes(currentDirectory), path);
+        rootLength = getRootLength(path);
+    }
+
+    return normalizedPathComponents(path, rootLength);
+}
+
+export function combinePaths(path1: string, path2: string) {
+    if (!(path1 && path1.length)) return path2;
+    if (!(path2 && path2.length)) return path1;
+    if (getRootLength(path2) !== 0) return path2;
+    if (path1.charAt(path1.length - 1) === directorySeparator) return path1 + path2;
+    return path1 + directorySeparator + path2;
+}
+
+function normalizedPathComponents(path: string, rootLength: number) {
+    const normalizedParts = getNormalizedParts(path, rootLength);
+    return [path.substr(0, rootLength)].concat(normalizedParts);
+}
+
+/** A path ending with '/' refers to a directory only, never a file. */
+export function pathEndsWithDirectorySeparator(path: string): boolean {
+    return path.charCodeAt(path.length - 1) === directorySeparatorCharCode;
+}
